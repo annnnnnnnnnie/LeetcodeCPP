d05c72fc9ce8510c62a1860a17b23118cd588b9b
PanTaixi <2330610648@qq.com>
2022-03-09 22:45:40 +0800

    Implemented matrix method
    still overtime

--- LeetcodeCPP/Solution_no798.h
+++ LeetcodeCPP/Solution_no798.h
@@ -1,10 +1,14 @@
 #ifndef SOL_NO798_H
 #define SOL_NO798_H
 
-#include<vector>
-#include<iostream>
+#include <vector>
+#include <numeric>
+#include <iostream>
 using namespace std;
 
+/***
+ * https://leetcode-cn.com/problems/smallest-rotation-with-highest-score/
+**/
 class Solution_no798
 {
 public:
@@ -13,14 +17,20 @@ public:
     }
 
     int bestRotationMatrix(vector<int>& nums) {
+        int n = nums.size();
+        int max_score = 0;
+        int best_k = 0;
+
         vector<vector<int>> matrix = generateScoreMatrix(nums);
-        for (auto row : matrix) {
-            for (auto num : row) {
-                cout << num << " ";
+
+        for (int i = 0; i < n; i++) {
+            int current_score = scoreArrayFromMatrix(matrix, i);
+            if (current_score > max_score) {
+                max_score = current_score;
+                best_k = i;
             }
-            cout << endl;
         }
-        return 0;
+        return best_k;
     }
 
     int bestRotationDirect(vector<int>& nums) {
@@ -50,8 +60,10 @@ public:
         return score;
     }
 
-    int scoreArrayFromMatrix(vector<vector<int>>& scoreMatrix) {
-        return 0;
+    int scoreArrayFromMatrix(vector<vector<int>>& scoreMatrix, int start) {
+        vector<int>& row = scoreMatrix[start];
+        int score = accumulate(row.begin(), row.end(), 0);
+        return score;
     }
 
     // Generate a matrix that represents the score each element might get
@@ -65,14 +77,18 @@ public:
         return matrix;
     }
 
+    int mod(int i, int n) {
+        return ((i % n) + n) % n;
+    }
+
     // precondition: matrix is initialized with zero. 
     // precondition: 0 <= num < k.
     void fillInMatrixColumn(vector<vector<int>>& matrix, int col_number, int num) {
         int n = matrix.size();
-        int row_begin = (col_number - num - n) % n;
-        int row_end = col_number;
-        for (int i = row_begin; (i - 1) % n != row_end; i = (i++) % n) {
-            matrix[i][col_number] = 1;
+        int row_begin = col_number + 1;
+        int cnt = n - num;
+        for (int i = 0; i < cnt; i++) {
+            matrix[(row_begin + i) % n][col_number] = 1;
         }
     }
 };
--- LeetcodeCPPTests/TestSolutionNo798.cpp
+++ LeetcodeCPPTests/TestSolutionNo798.cpp
@@ -2,7 +2,52 @@
 #include "CppUnitTest.h"
 #include "../LeetcodeCPP/Solution_no798.h"
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+#include <iostream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework; 
+
+namespace Microsoft {
+	namespace VisualStudio {
+		namespace CppUnitTestFramework {
+			template<>
+			static std::wstring ToString<pair<int, int>>(const pair<int, int>& position) {
+				wstring result = L"pair:(";
+				result += to_wstring(position.first);
+				result += L", ";
+				result += to_wstring(position.second);
+				result += L")";
+				return result;
+			}
+
+			template<>
+			static std::wstring ToString<vector<vector<int>>>(const vector<vector<int>>& matrix) {
+				wstring result = L"matrix: [";
+				for (vector<int> row : matrix) {
+					result += L"[";
+					for (int n : row) {
+						result += to_wstring(n) + L", ";
+					}
+					result += L"],\n";
+				}
+				result += L"]";
+				return result;
+			}
+
+			template<>
+			static std::wstring ToString<vector<char>>(const vector<char>& ls) {
+				wstring result = L"[";
+				for (char c : ls) {
+					result += to_wstring(c) + L", ";
+				}
+				return result;
+			}
+			template<>
+			static std::wstring ToString<Solution_no798>(const class Solution_no798& t) {
+				return L"Solution_no798";
+			}
+		}
+	}
+}
 
 namespace LeetcodeCPPTests
 {
@@ -11,10 +56,60 @@ namespace LeetcodeCPPTests
 	public:
 		Solution_no798 sol;
 
+		static std::string matrixToString(const vector<vector<int>>& matrix) {
+			string result = "matrix: [";
+			for (vector<int> row : matrix) {
+				result += "[";
+				for (int n : row) {
+					result += to_string(n) + ", ";
+				}
+				result += "],\n";
+			}
+			result += "]";
+			return result;
+		}
+
+		void assertMatrixEqual(vector<vector<int>>& m1, vector<vector<int>>& m2) {
+			int n1 = m1.size();
+			int n2 = m2.size();
+			Assert::AreEqual(n1, n2);
+
+			for (int i = 0; i < n1; i++) {
+				vector<int>& r1 = m1[i];
+				vector<int>& r2 = m2[i];
+				int nr1 = r1.size();
+				int nr2 = r2.size();
+				Assert::AreEqual(nr1, nr2);
+
+				for (int j = 0; j < nr1; j++) {
+					Assert::AreEqual(r1[j], r2[j]);
+				}
+			}
+		}
 		TEST_METHOD(TestGenerateMatrix)
 		{
 			vector<int> nums{ 2,3,1,4,0 };
 			auto matrix = sol.generateScoreMatrix(nums);
+			string mat = matrixToString(matrix);
+			vector<vector<int>> answer{
+				{0,0,1,0,1},
+				{1,0,1,0,1},
+				{1,1,0,0,1},
+				{1,1,1,0,1},
+				{0,0,1,1,1}
+			};
+			assertMatrixEqual(answer, matrix);
+		}
+
+		TEST_METHOD(TestUseMatrixToComputeCaseOne) {
+			vector<int> nums{ 2,3,1,4,0 };
+			int result = sol.bestRotationMatrix(nums);
+			int answer = 3;
+			Assert::AreEqual(answer, result);
+		}
+
+		TEST_METHOD(TestLargeData) {
+			vector<int> nums;
 
 		}
 	};
